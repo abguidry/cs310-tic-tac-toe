e55a0b9baa4c94e9d3a8e97c42253ded6ca9eada
Aubrey G <dearaubrey1@gmail.com>
2019-02-11 17:12:08 -0600

    Gui update

--- src/main/java/edu/jsu/mcis/TicTacToeController.java
+++ src/main/java/edu/jsu/mcis/TicTacToeController.java
@@ -4,36 +4,35 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
 
-public class TicTacToeController implements ActionListener {
+public class TicTacToeController implements ActionListener{
 
     private final TicTacToeModel model;
     private final TicTacToeView view;
 
     /* CONSTRUCTOR */
 
-    public TicTacToeController {
+    public TicTacToeController(int width) {
 
         /* Initialize model, view, and width */
 
         model = new TicTacToeModel(width);
-        view = new TicTacToeView();
+        view = new TicTacToeView(this, width);
 
     }
 
     public String getMarkAsString(int row, int col) {
         return (model.getMark(row, col).toString());
-      }
+    }
 
-      public TicTacToeView getView(){
+    public TicTacToeView getView() {
         return view;
-      }
+    }
 
-      @Override
-    public void actionPerformed(ActionEvent event){
-        Jbutton button = null;
-        if(event.getSource() instanceof Jbutton){
-          button = (JButton)(event.getSource());
-
+    @Override
+    public void actionPerformed(ActionEvent event) {
+        JButton button = null;
+        if (event.getSource() instanceof JButton) {
+            button = (JButton)(event.getSource());
         }
         String[] coordinates = button.getName().replace("Square", "").split("");
         model.makeMark(Integer.parseInt(coordinates[0]), Integer.parseInt(coordinates[1]));
@@ -44,4 +43,5 @@ public class TicTacToeController implements ActionListener {
         }
 
     }
+
 }
